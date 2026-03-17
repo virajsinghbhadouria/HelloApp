37bca8dc6fa20ea131110031c79a47ea5551d145
unknown <vs9771@srmist.edu.in>
2026-03-17 08:59:16 +0530

    helloapp

--- helloapp.java
+++ helloapp.java
@@ -1,9 +1,16 @@
 public class HelloApp {
-	public static void main(String[]args){
-		String name = "World";
-		if (args.length>0){
-			name = args[0];
-		}
-		system.out.println("Hello, " + name + "!");
-	}
-	}
+    public static void main(String[] args) {      
+        String result = "Hello, World!";  
+        if (args.length > 0) {
+            StringBuilder names = new StringBuilder();         
+            for (int i = 0; i < args.length; i++) {
+                names.append(args[i]);
+                if (i < args.length - 1) {
+                    names.append(", ");
+                }
+            }
+            result = "Hello, " + names.toString() + "!";
+        } 
+       System.out.println(result);
+    }
+}
