761ba14dad36f6366e6d93745756a19102047d8b
unknown <vs9771@srmist.edu.in>
2026-03-17 08:40:56 +0530

    helloapp

--- helloapp.java
+++ helloapp.java
@@ -1,5 +1,6 @@
-public class helloapp {
-   public static void main (string[]args){
-     system.out.println(x: "Hello, world!");
-   }
- }
+public class HelloApp {
+	public static void main(string[]args) {
+		string name = args[0];
+		system.out.println("Hello, " + name + "!");
+	}
+}
